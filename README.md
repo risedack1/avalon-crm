c8eaab1dbd5ae030f864b39856c47f1755e3ceee
risedack1 <risedack1@gmail.com>
2023-07-08 23:08:37 +0200

    start project

---
+++ README.md
@@ -0,0 +1,5 @@
+# js-mpa-vite-template
+
+# Avalon CRM system
+
+<p align="center">В качестве сборщика используется <a href="https://vitejs.dev/"><b>Vite JS<b></a></p>
---
+++ vite.config.js
@@ -0,0 +1,74 @@
+import { defineConfig } from "vite";
+import { createHtmlPlugin } from "vite-plugin-html";
+import viteImagemin from "vite-plugin-imagemin";
+import { VitePluginFonts } from "vite-plugin-fonts";
+import legacy from "@vitejs/plugin-legacy";
+import StylelintPlugin from "vite-plugin-stylelint";
+import eslint from "vite-plugin-eslint";
+
+export default defineConfig({
+  plugins: [
+    createHtmlPlugin({
+      minify: false,
+      pages: [
+        {
+          entry: "src/js/main.js",
+          filename: "index.html",
+          template: "index.html",
+        },
+      ],
+    }),
+    viteImagemin({
+      gifsicle: {
+        optimizationLevel: 7,
+        interlaced: false,
+      },
+      optipng: {
+        optimizationLevel: 7,
+      },
+      mozjpeg: {
+        quality: 20,
+      },
+      pngquant: {
+        quality: [0.8, 0.9],
+        speed: 4,
+      },
+      svgo: {
+        plugins: [
+          {
+            name: "removeViewBox",
+          },
+          {
+            name: "removeEmptyAttrs",
+            active: false,
+          },
+        ],
+      },
+    }),
+    VitePluginFonts({
+      google: {
+        preconnect: true,
+        display: "swap",
+        text: "ViteAwsom",
+        injectTo: "head-prepend",
+        families: [
+          {
+            name: "Roboto",
+            styles: "ital,wght@0,400;1,200",
+            defer: true,
+          },
+        ],
+      },
+    }),
+    legacy({
+      targets: ["defaults", "not IE 11"],
+    }),
+    StylelintPlugin({
+      files: ["src/scss/**/*.scss"],
+    }),
+    eslint({
+      include: ["src/**/*.js"],
+      exclude: ["node_modules/**", "dist/**"],
+    }),
+  ],
+});
