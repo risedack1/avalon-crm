cc2c5f2ee27ac376afcdefd9201fac47708f440e
risedack1 <risedack1@gmail.com>
2023-07-10 22:31:19 +0200

    profile modal

--- src/js/main.js
+++ src/js/main.js
@@ -2,3 +2,5 @@ import "../scss/style.scss";
 import "./components/aside";
 import "./components/burger-menu";
 import "./components/table";
+import "./components/modals";
+import "./components/custom-select";
